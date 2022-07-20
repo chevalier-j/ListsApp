468fee8ac600b38f706389e4d8f1b5871f9136bf
username <adelgibert@gmail.com>
2022-07-20 18:17:28 +1200

    Initial Commit

---
+++ ListsApp/ListsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListsAppApp.swift
+//  ListsApp
+//
+//  Created by x on 20/07/22.
+//
+
+import SwiftUI
+
+@main
+struct ListsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
